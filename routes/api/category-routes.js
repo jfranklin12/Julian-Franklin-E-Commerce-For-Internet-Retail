5807ff3cc5cc11e567ba133e3f00cf3319f75ece
jfranklin12 <j.franks725@gmail.com>
2022-07-28 16:53:40 -0400

    updated put request for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,12 +29,33 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
   // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+  const updatedCategory = await Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.status(200).json(updatedCategory)
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
